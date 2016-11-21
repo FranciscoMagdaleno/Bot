1671f48abee0e2a662d11504f09176f4a6f598bf
FranciscoMagdaleno <francisco.magdalenog@gmail.com>
2016-11-21 16:37:00 +0100

    New version

--- Bot.py
+++ Bot.py
@@ -1,43 +1,41 @@
 import requests
 import json
+import smtplib
+ 
+fromaddr = raw_input('From: ')
+toaddrs  = raw_input('To: ')
+
 
 url = 'http://es.wallapop.com/rest/items?kws=sony&_p=0&catIds=&lat=41.398077&lng=2.170432&minPrice=&maxPrice=&order=&dist='
-
-r = requests.get(url)
-data = r.json()
-x=len(data['items'])
-
-'''Para la pagina 0'''
-
-print '------------ Pagina 0 ------------'
-for i in range(x):
-    print 'producto->',data['items'][i]['title']
-    print 'descripcion->',data['items'][i]['description']
-    print 'precio->',data['items'][i]['salePrice']
-    print ''
-    
-'''Para el resto de paginas'''
-
-for paginaIndex in range(1,10):
-    print '------------ Pagina '+str(paginaIndex)+' ------------'   
-    url = url.replace('_p='+str(paginaIndex-1),'_p='+str(paginaIndex))
+limitPrice = 100
+msg = ''
+for paginaIndex in range(5):
+    head = '------------ pagina ' +str(paginaIndex)+ ' ------------'
+    url = url.replace('_p=0','_p='+str(paginaIndex))
     r = requests.get(url)    
     data = r.json()
+    msg = msg + '\n' + head + '\n'
     x=len(data['items'])
     for i in range(x):
-        print 'producto->',data['items'][i]['title']
-        print 'descripcion->',data['items'][i]['description']
-        print 'precio->',data['items'][i]['salePrice']
-        print ''
+        title = data['items'][i]['title'].encode('utf8')
+        auxTitle = title.lower()
+        #print title
+        description = data['items'][i]['description'].encode('utf8')
+        #print description
+        price = data['items'][i]['salePrice']
+        #print price
+        #print ''
+        if ('sony' in auxTitle) and (price < limitPrice):
+            msg = msg + '\n' + 'producto->' + str(title) + '\n' + 'descripcion->' + str(description) + '\n' + 'precio->' + str(price) + '\n' 
 
-
-    
-
-
-
-
-
-
-
-
+# Datos
+username = fromaddr
+password = raw_input('Contraseña: ')
+ 
+# Enviando el correo
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username,password)
+server.sendmail(fromaddr, toaddrs, msg)
+server.quit()
 
